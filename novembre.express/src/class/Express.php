1c9f6344fa23069f06a87777240d4a81b2da8b6a
Djib <coudert@novembre.com>
2017-06-29 11:02:11 +0200

    chore: add useful functions for WP Express

--- novembre.express/src/class/Express.php
+++ novembre.express/src/class/Express.php
@@ -2,8 +2,18 @@
 
 class Express {
 
+    private $path_img = "/dist/assets/img/";
+    
+
+    public static function get_img($pathFromTheme='')
+    {
+        return get_template_directory_uri() . self::$path_img . $pathFromTheme;
+    }
+
     public static function img($pathFromTheme="") {
 
-        echo get_template_directory_uri().'/dist/assets/img/'.$pathFromTheme;
+        echo get_template_directory_uri() . self::$path_img . $pathFromTheme;
     }
+
+
 }
