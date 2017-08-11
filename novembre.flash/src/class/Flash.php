fe606e8de434270c3903bd3b0dc10f02418b8fa9
Djib <coudert@novembre.com>
2017-08-11 15:54:55 +0200

    add: Flash message interface

---
+++ novembre.flash/src/class/Flash.php
@@ -0,0 +1,24 @@
+<?php namespace Novembre\Flash;
+
+class Flash implements SessionInterface, \Countable {
+
+    public function get($key) 
+    {
+
+    }
+
+    public function set($key, $value) 
+    {
+
+    }
+
+    public function delete($key) 
+    {
+
+    }
+
+    public function count($var, int $mode = COUNT_NORMAL) 
+    {
+        return 4;
+    }
+}
---
+++ novembre.flash/src/class/SessionInterface.php
@@ -0,0 +1,10 @@
+<?php namespace Novembre\Flash;
+
+interface SessionInterface {
+
+    public function get($key);
+
+    public function set($key, $value);
+
+    public function delete($key);
+};
